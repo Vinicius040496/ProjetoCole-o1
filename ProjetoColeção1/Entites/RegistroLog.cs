5c3ff1f9a5105e7c7a3c592701973de3c3adce7d
Vinicius Almeida <vinicius.almeida2011@gmail.com>
2024-07-31 14:14:13 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoColeção1/Entites/RegistroLog.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoColeção1.Entites
+{
+    internal class RegistroLog
+    {
+        public string NomeUsuario { get; set; }
+        public DateTime Momento { get; set; }
+
+        public override int GetHashCode()
+        {
+            return NomeUsuario.GetHashCode();
+        }
+
+        public RegistroLog(string nomeUsuario, DateTime momento)
+        {
+            NomeUsuario = nomeUsuario;
+            Momento = momento;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is RegistroLog)) { 
+                return false;
+            }
+            else
+            {
+                RegistroLog other = (RegistroLog) obj;
+                return NomeUsuario == other.NomeUsuario;
+            }
+            
+        
+        }
+    }
+}
---
+++ ProjetoColeção1/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using ProjetoColeção1.Entites;
+namespace ProjetoColeção1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<RegistroLog> registroLogs = new HashSet<RegistroLog>();
+            Console.Write("Entre com pasta do arquivo: ");
+            string pasta = Console.ReadLine();
+            try
+            {
+                using (StreamReader sr = File.OpenText(pasta))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] linha = sr.ReadLine().Split(' ');
+                        string nome = linha[0];
+                        DateTime momento = DateTime.Parse(linha[1]);
+                        registroLogs.Add(new RegistroLog(nome, momento));
+                        Console.WriteLine("Total usuarios: " + registroLogs.Count);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
